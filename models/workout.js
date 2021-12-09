fc1b488edf99e1a239df26f94cfbb512a34b7f15
Hardik Rola <hrsautomation20@gmail.com>
2021-12-09 09:30:37 -0500

    Created Workout Schema

---
+++ models/workout.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+// Mongoose Schema
+const Schema = mongoose.Schema;
+
+// Creating new Workout Schema
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        enum: ["resistance", "cardio"],
+        required: "Enter Exercise Name",
+      },
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter Exercise Type",
+      },
+      distance: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+        required: "Enter Exercise Duration",
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+// Export workout model
+module.exports = Workout;
