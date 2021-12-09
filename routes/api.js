d48bf5acb47531030222749afa2213d9d2af78c1
Hardik Rola <hrsautomation20@gmail.com>
2021-12-09 09:31:09 -0500

    Created API routes for CURD Operation

---
+++ routes/api.js
@@ -0,0 +1,70 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+// GET workout
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// ADD exercise
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  console.log("check!", body, params);
+
+  Workout.findByIdAndUpdate(
+    { _id: params.id },
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).res.json(err);
+    });
+});
+
+// CREATE new workout
+router.post("/api/workouts", ({ body }, res) => {
+  console.log("ok");
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).res.json(err);
+    });
+});
+
+// GET workout in 7 day
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// Export API routes
+module.exports = router;
